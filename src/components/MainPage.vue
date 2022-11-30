7937be168199902bda6c1cc82955912ffd60aee7
MicheleRai <rainonemichele1999@gmail.com>
2022-11-30 15:54:08 +0100

    axios added

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -5,8 +5,24 @@
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
   name: 'MainPage',
+  data() {
+    return {
+      urlApi: 'https://api.themoviedb.org/3/movie/550?api_key=ce50709de8103dbd86e95807b64b0b75',
+      arrFilm: null,
+    };
+  },
+  created() {
+    axios.get(this.urlApi)
+      .then((axiosResponse) => {
+        console.log(axiosResponse);
+        // this.arrFilm = axiosResponse.data
+      });
+  },
 };
 </script>
 
