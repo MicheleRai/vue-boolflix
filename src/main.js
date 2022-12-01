23f114f839e4630b4fcbef63990a2c13b6c25bdd
MicheleRai <rainonemichele1999@gmail.com>
2022-12-01 18:19:49 +0100

    star imported

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core'; // libreria icone del progetto
+import { faChevronRight, faHatWizard, faStar } from '@fortawesome/free-solid-svg-icons'; // importiamo le icone che ci servono
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; // importazione del componente FontAwesomeIcon
 import App from './App.vue';
+
+library.add(faChevronRight, faHatWizard, faStar);
+Vue.component('FontAwesomeIcon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+<i class="fa-solid fa-star"></i>;
