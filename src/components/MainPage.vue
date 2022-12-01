8cf6c2224a91415e9c59812aa875d95d2df72482
MicheleRai <rainonemichele1999@gmail.com>
2022-12-01 19:58:01 +0100

    hover effect added

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,23 +7,41 @@
       >
         MOVIE
       </h1>
-      <ul
+      <div
         v-for="objMovie in arrMovies"
         :key="objMovie.id"
+        class="container"
       >
         <img
           :src="'https://image.tmdb.org/t/p/w342/' + objMovie.poster_path"
           alt="img"
         >
-        <li><h1>{{ objMovie.title }}</h1></li>
-        <li>Titolo originale:{{ objMovie.original_title }}</li>
-        <li>
-          Lingua originale:<lang-flag
-            :iso="objMovie.original_language"
-          />
-        </li>
-        <li>Voto:{{ objMovie.vote_average }}</li>
-      </ul>
+        <ul>
+          <li><h1>{{ objMovie.title }}</h1></li>
+          <li><strong>Titolo originale:</strong>{{ objMovie.original_title }}</li>
+          <li>
+            <strong>Lingua originale:</strong><lang-flag
+              :iso="objMovie.original_language"
+            />
+          </li>
+          <li>
+            <strong>Voto:</strong>
+            <font-awesome-icon
+              v-for="stars in (parseInt(objMovie.vote_average / 2))"
+              :key="stars"
+              icon="fa-solid fa-star"
+              style="color:gold"
+            />
+            <font-awesome-icon
+              v-for="stars in (5 - parseInt(objMovie.vote_average / 2))"
+              :key="(stars + 5)"
+              icon="fa-regular fa-star"
+              style="color:white"
+            />
+          </li>
+          <li><strong>Trama:</strong> {{ objMovie.overview }}</li>
+        </ul>
+      </div>
       <h1
         v-if="arrTv.length"
         class="section"
@@ -47,7 +65,8 @@
             :iso="objTv.original_language"
           />
         </li>
-        <li>Voto:{{ objTv.vote_average }}</li>
+        <li><strong>Trama:</strong> {{ objTv.overview }}</li>
+        <li><strong>Voto:</strong>{{ objTv.vote_average }}</li>
       </ul>
     </div>
   </main>
@@ -69,6 +88,9 @@ export default {
     return {
     };
   },
+  methods: {
+
+  },
 };
 </script>
 
@@ -82,21 +104,35 @@ export default {
     flex-wrap: wrap;
     div{
       display: flex;
-      text-align: center;
       justify-content: center;
       flex-wrap: wrap;
       .section{
         flex: 0 0 100%;
+        text-align: center;
       }
-      ul{
-        list-style: none;
+      .container{
         margin: 1rem;
-        flex: 0 0 20%;
-       li{
-         background-color: rgb(165, 161, 161);
-
-       }
+        flex: 0 0 26.3%;
+        border: 2px solid white;
+        ul{
+          list-style: none;
+          height: 80%;
+          display: none;
+          align-items: flex-start;
+          flex-direction: column;
+          color: white;
+          padding: 1.5rem;
+        }
       }
+      .container:hover img{
+          display: none;
+        }
+        .container:hover ul{
+          display: flex;
+        }
+        .container:hover{
+          background-color: black;
+        }
     }
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import Vue from 'vue';
-import { library } from '@fortawesome/fontawesome-svg-core'; // libreria icone del progetto
-import { faChevronRight, faHatWizard, faStar } from '@fortawesome/free-solid-svg-icons'; // importiamo le icone che ci servono
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'; // importazione del componente FontAwesomeIcon
 import App from './App.vue';
 
-library.add(faChevronRight, faHatWizard, faStar);
 Vue.component('FontAwesomeIcon', FontAwesomeIcon);
-
+library.add(faStar, regularStar);
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
-<i class="fa-solid fa-star"></i>;
